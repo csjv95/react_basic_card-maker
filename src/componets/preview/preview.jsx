4d6ef7fb0de2d16ee20e178a9856d9b752e33e88
csjv95 <csjv95@gmail.com>
2021-02-20 10:10:26 +0900

    Styling editor & preview

---
+++ src/componets/preview/preview.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './preview.module.css'
+
+const Preview = (props) => {
+  return(
+    <section className={styles.preview}>
+      <h3 className={styles.title}>Card Preview</h3>
+    </section>
+  )
+}
+
+export default Preview;
