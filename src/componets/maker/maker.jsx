10bc987bde51714d0af91b88c72aeff8c83c28e2
csjv95 <csjv95@gmail.com>
2021-03-02 17:54:29 +0900

    rember user (sync cards) in realtime databases

--- src/componets/maker/maker.jsx
+++ src/componets/maker/maker.jsx
@@ -10,39 +10,48 @@ const Maker = ({ authService, FileInput, cardRepository }) => {
   const history = useHistory();
   const historyState = history?.location?.state;
   const [cards, setCards] = useState({});
-  const [userId, setUserId] =useState(historyState && historyState.id)
+  const [userId, setUserId] = useState(historyState && historyState.id);
 
   const onLogout = () => {
     authService.logout();
   };
 
   useEffect(() => {
+    if (!userId) {
+      return;
+    }
+    const stopSync = cardRepository.syncCards(userId, (cards) => {
+      setCards(cards);
+    });
+    return () => stopSync();
+  }, [userId]);
+
+  useEffect(() => {
     authService.onAuthChange((user) => {
       if (user) {
-        setUserId(user.uid)
-        console.log(userId);
-      }else {
+        setUserId(user.uid);
+      } else {
         history.push("/");
       }
     });
   });
 
   const createOrUpdateCard = (card) => {
-    setCards(cards => {
-      const updated = {...cards};
+    setCards((cards) => {
+      const updated = { ...cards };
       updated[card.id] = card;
       return updated;
-    })
-    cardRepository.saveCard(userId, card); 
+    });
+    cardRepository.saveCard(userId, card);
   };
 
   const deleteCard = (card) => {
-    setCards(cards => {
-      const updated = {...cards};
+    setCards((cards) => {
+      const updated = { ...cards };
       delete updated[card.id];
       return updated;
-    })
-    cardRepository.removeCard(userId, card); 
+    });
+    cardRepository.removeCard(userId, card);
   };
 
   return (
